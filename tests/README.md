5a203ecbf661a64384b5d23cb3810c9cf9121268
Wahieb Talal <49795325+wahebtalal@users.noreply.github.com>
2025-09-30 18:43:42 +0300

    Delete tests directory

--- tests/README.md
+++
@@ -1,3 +0,0 @@
-# FlutterFire end-to-end testing application.
-
-All package e2e tests belong in this application.
--- tests/android/app/src/main/kotlin/io/flutter/plugins/firebase/tests/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package io.flutter.plugins.firebase.tests
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
